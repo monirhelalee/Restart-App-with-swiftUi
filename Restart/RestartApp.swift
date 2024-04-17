ac9ade7b300556e56156eec304e27225fb4941d5
monir <mdmonirhelale@gmail.com>
2024-04-17 23:39:18 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Monir Haider Helalee on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
