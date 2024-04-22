410704261c11c7aade7179c57d19586546acdae3
monir <mdmonirhelale@gmail.com>
2024-04-22 23:32:36 +0600

    designe onbording page

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,15 +12,98 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardinViewActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20){
-            Text("Onbording")
-                .font(.largeTitle)
-            
-            Button{
-                //Action
-                isOnboardinViewActive = false
-            }label: {
-                Text("start")
+        
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all,edges: .all)
+            VStack(spacing: 20){
+                // MARK - HADER
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but how much love we put into given.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal,10)
+                }
+                
+                // MARK - CENTER
+                
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                Spacer()
+                
+                // MARK - Fotter
+                
+                ZStack{
+                    // PART OF THE CUSTOM BUTTON
+                    
+                    // 1. Background (STATIC)
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    // 2. Call - to - action (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title3,design: .rounded))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    // 3. capsule (dynamic widgt)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    // 4. circle (draggable)
+                    
+                    HStack {
+                        ZStack{
+                           Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24,weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80,height: 80,alignment: .center)
+                        .onTapGesture {
+                            isOnboardinViewActive = false
+                        }
+                        
+                        Spacer()
+                        
+                    }
+                }
+                .frame(height: 80,alignment: .center)
+                .padding()
             }
         }//: VSTACK
     }
